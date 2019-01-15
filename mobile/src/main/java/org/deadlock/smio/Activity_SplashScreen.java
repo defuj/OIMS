0b87c74f9cd9d545e9e09797e51f69928697e572
Dede Fuji <defuj.official@gmail.com>
2019-01-15 22:44:46 +0700

    Initial commit

---
+++ mobile/src/main/java/org/deadlock/smio/Activity_SplashScreen.java
@@ -0,0 +1,13 @@
+package org.deadlock.smio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
